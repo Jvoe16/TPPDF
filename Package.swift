ffcb84469c63fdddb1c9b4a9d9528c176c1e762c
Jesús Orduño <jvoe01@me.com>
2019-10-19 15:53:12 -0600

    Swift Package Manager compatibility

--- Package.swift
+++ Package.swift
@@ -6,17 +6,20 @@ import PackageDescription
 let package = Package(
     name: "TPPDF",
     platforms: [
-        .iOS(.v12)],
+        .iOS(.v8)],
     products: [
         // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "TPPDF",
             targets: ["TPPDF"]),
     ],
+    dependencies: [
+           .package(url: "https://github.com/realm/SwiftLint.git", from: "0.35.0"),
+       ],
     targets: [
             // Targets are the basic building blocks of a package. A target can define a module or a test suite.
             // Targets can depend on other targets in this package, and on products in packages which this package depends on.
             .target(
-                name: "TPPDF")
+                name: "TPPDF", dependencies: ["swiftlint"]),
     ]
 )
