31268acc47b13019d14865d450ae09672d86f632
Jvoe16 <56768946+Jvoe16@users.noreply.github.com>
2019-10-19 11:58:47 -0600

    Swift Package Manager Compatibility

--- Package.swift
+++ Package.swift
@@ -13,19 +13,10 @@ let package = Package(
             name: "TPPDF",
             targets: ["TPPDF"]),
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-        .package(
-            url: "https://github.com/realm/SwiftLint.git",
-            from: "0.35.0"
-        )
-    ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
-            name: "TPPDF",
-            dependencies: ["SwiftLint"])
+            // Targets are the basic building blocks of a package. A target can define a module or a test suite.
+            // Targets can depend on other targets in this package, and on products in packages which this package depends on.
+            .target(
+                name: "TPPDF")
     ]
 )
