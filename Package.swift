7593b09186deb040a51deddf77678875174235d5
Jesús Orduño <jvoe01@me.com>
2019-10-19 16:57:02 -0600

    Swift Package Manager compatibility test

--- Package.swift
+++ Package.swift
@@ -14,14 +14,15 @@ let package = Package(
             targets: ["TPPDF"]),
     ],
     dependencies: [
-        .package(path: "/SwifLint/source/swiftlint")
+        .package(
+        url: "https://github.com/realm/SwiftLint.git", from: "0.35.0")
        ],
     targets: [
             // Targets are the basic building blocks of a package. A target can define a module or a test suite.
             // Targets can depend on other targets in this package, and on products in packages which this package depends on.
             .target(
                 name: "TPPDF",
-                dependencies: ["swiftlint"]
+                dependencies: ["swiftlint", "SwiftLintFramework"]
         )
     ]
 )
